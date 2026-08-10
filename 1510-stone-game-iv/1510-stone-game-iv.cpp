22a1aedcbd7be872d2e26cccdd0e9e142f737949
Anubhav Bhardwaj <61623726+Myth80@users.noreply.github.com>
2026-08-10 20:18:34 +0530

    Time: 36 ms (69.29%), Space: 9 MB (82.55%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    static constexpr int MAX = 100000;
-    inline static bitset<MAX + 1> dp;
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n + 1, false);
 
-    inline static bool init = []() {
-        for (int i = 0; i <= MAX; i++) {
-            if (dp.test(i)) continue;
-            for (int j = 1; j * j <= MAX - i; j++)
-                dp.set(i + j * j);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                // If we can move to a losing state,
+                // current player can force a win.
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
         }
-        
-        return false;
-    }();
 
-    bool winnerSquareGame(int n) { return dp.test(n); }
+        return dp[n];
+    }
 };
