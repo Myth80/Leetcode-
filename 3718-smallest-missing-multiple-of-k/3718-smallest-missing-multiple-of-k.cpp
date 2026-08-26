5b09efb18e5c0255c83d4515a5b8ad46f58b1fea
Anubhav Bhardwaj <61623726+Myth80@users.noreply.github.com>
2026-08-26 19:35:40 +0530

    Time: 0 ms (100%), Space: 24.1 MB (66.93%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int> s(nums.begin(), nums.end());
         for(int x = k;; x+=k){
-            if(s.find(x)==s.end())return x;
+            if(find(nums.begin(),nums.end(),x)==nums.end())return x;
+
         }
     }
 };
