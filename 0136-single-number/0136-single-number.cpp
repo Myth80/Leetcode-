16b18f5871c72243b6d7b64fec1709abef1d29cd
Anubhav Bhardwaj <61623726+Myth80@users.noreply.github.com>
2026-09-19 22:02:47 +0530

    Time: 12 ms (11.69%), Space: 23.9 MB (7.31%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int , int>freq;
+        for(auto x: nums){
+            freq[x]++;
+
+        }
+        for(auto x : freq){
+            if(x.second==1){
+                return x.first;
+            }
+        }
+        return 0;
+    }
+};
